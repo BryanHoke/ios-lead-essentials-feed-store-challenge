7731667cf001c5a35fb6731adda115eced135b49
Bryan Hoke <zinth@icloud.com>
2021-08-11 13:51:55 -0500

    Ensure that `CoreDataFeedStore.insert` overwrites previously inserted cache values.

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -47,7 +47,7 @@ public final class CoreDataFeedStore: FeedStore {
 		let context = self.context
 		context.perform {
 			do {
-				let managedCache = ManagedCache(context: context)
+				let managedCache = try ManagedCache.newUniqueInstance(in: context)
 				managedCache.timestamp = timestamp
 				managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
 				try context.save()
@@ -73,6 +73,11 @@ private class ManagedCache: NSManagedObject {
 	static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
 		let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
 		return try context.fetch(request).first
+	}
+
+	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+		try find(in: context).map(context.delete)
+		return ManagedCache(context: context)
 	}
 
 	var localFeed: [LocalFeedImage] {

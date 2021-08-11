1f721cafad9896f8c0fb51576ec555e71716571f
Bryan Hoke <zinth@icloud.com>
2021-08-11 13:49:07 -0500

    Extract cache fetch request logic into a static method on `ManagedCache`.

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -32,8 +32,7 @@ public final class CoreDataFeedStore: FeedStore {
 		let context = self.context
 		context.perform {
 			do {
-				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
-				if let cache = try context.fetch(request).first {
+				if let cache = try ManagedCache.find(in: context) {
 					completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
 				} else {
 					completion(.empty)
@@ -70,6 +69,11 @@ extension NSPersistentContainer {}
 private class ManagedCache: NSManagedObject {
 	@NSManaged var timestamp: Date
 	@NSManaged var feed: NSOrderedSet
+
+	static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+		let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+		return try context.fetch(request).first
+	}
 
 	var localFeed: [LocalFeedImage] {
 		feed.compactMap { ($0 as? ManagedFeedImage)?.local }
